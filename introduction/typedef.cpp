c6581c9c8e395ea00d940563550ee98ffc938c7b
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-05-17 20:33:10 +0500

    feat: add typedef

---
+++ introduction/typedef.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    // before
+    long long longVar;
+
+    // after
+    typedef long long ll;
+    ll longVarShort;
+
+    return 0;
+}
