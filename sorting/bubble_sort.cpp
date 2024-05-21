36d7055887996ef1d70f6b7622543c15b77e0fa9
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-05-21 10:04:46 +0500

    feat: bubble sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
+void bubbleSort(int* list,int n);
 int main(){
+    /// given array
+    int list[] = {1,-5,2,0,3,4};
+    /// size of array 
+    int n = sizeof(list)/sizeof(list[0]);
+    /// sorting
+    bubbleSort(list,n);
+    /// result
+    cout<<"Sorted list: \n";
+    for (int i=0;i<n;i++){
+        cout<<list[i]<<" ";
+    }
 
     return 0;
+}
+
+void bubbleSort(int* list,int n){
+    for (int i =0 ; i< n-1;i++ ){
+        for (int j=1 ; j< n; j++ ){
+            if(list[j]<list[j-1]){
+                int item1 = list[j];
+                list[j] = list[j-1];
+                list[j-1]= item1;
+            }
+        }
+    }
 }
